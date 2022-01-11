d344ad2f6f8815f72f57f883588e41d91b3c8750
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-01-11 18:34:18 +1300

    Create command_line.py

---
+++ app/command_line.py
@@ -0,0 +1,17 @@
+from app.controller import Controller
+
+
+class CommandLine:
+
+    def __init__(self, controller : Controller):
+        self.controller = controller
+        pass
+
+    def start(self):
+        pass
+
+    def end(self):
+        pass
+
+    def print_state(self):
+        pass
