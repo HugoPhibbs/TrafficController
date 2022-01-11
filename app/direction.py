4c1f8a6390ce23864d4ef0b6504f371ffe6db419
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-01-11 18:39:36 +1300

    Update direction.py
    Added a bunch of documentation, still need to do some more
    
    added a parameter for cycle size, which allows for more variation and experimentation. Avoids hard coding this into the class
    
    changed waiting times to be a ndarray from the start, makes things easy. Made changes to reflect this
    
    added a len method
    
    added setter and getter methods for cycle_size

--- app/direction.py
+++ app/direction.py
@@ -1,20 +1,50 @@
 from time import sleep
 import numpy as np
+from numpy import ndarray
+
 
 class Direction:
     next = None
+    __cycle_size: float
+    waiting_times: ndarray
 
-    def __init__(self, _name : str, _waiting_times : list = [], _avg_flow : int = 0):
-        self.__name : str = _name
-        self.waiting_times = _waiting_times
-        self.avg_flow : int = _avg_flow
+    def __init__(self, _name: str, _waiting_times: list = [], _avg_flow: int = 0, _cycle_size: int = 0.5):
+        """
+        Initializer for a Direction object
 
-    def empty(self, p) -> int:
+        :param _name: string for the name of this Direction for ease of identification
+        :param _waiting_times: list for the current waiting times for this direction
+        :param _avg_flow: int for the average flow for this Direction per cycle
+        :param _cycle_size: float for the proportion of vehicles to be emptied from this intersection per cycle
+        """
+        self.__name: str = _name
+        self.waiting_times = np.array(_waiting_times)
+        self.avg_flow: int = _avg_flow
+        self.cycle_size = _cycle_size
+
+    @property
+    def cycle_size(self):
+        return self.__cycle_size
+
+    @cycle_size.setter
+    def cycle_size(self, val: float):
+        """
+        Setter for the cycle size of this Direction
+
+        Must be a float between 0 and 1
+
+        :param val: float for the value to be set as the cycle size of this Direction
+        :return:
+        """
+        assert isinstance(val, float) and 0 < val < 1
+        self.__cycle_size = val
+
+    def empty(self, p) -> float:
         """
         Empties this direction for the required volume as specified by self.cycle_volume()
 
         :param p: the required time for a single vehicle to exit the intersection given a green light
-        :return: the total length of this cycle for this direciton. Used to add waiting times to the rest of
+        :return: float for the total length of this cycle for this direction. Used to add waiting times to the rest of
         vehicles that are still waiting their turn
         """
         cycle_volume = self.cycle_volume()
@@ -23,6 +53,14 @@ class Direction:
         sleep(cycle_durr)
         return cycle_durr
 
+    def __len__(self) -> int:
+        """
+        Returns the number of vehicles in this direction
+
+        :return: integer as described
+        """
+        return self.waiting_times.size
+
     def add_waiting_time(self, waiting_time) -> None:
         """
         Adds waiting time to each vehicle for this particular direction of traffic
@@ -30,9 +68,9 @@ class Direction:
         :param waiting_time: the waiting time to be added to all cars for this direction
         :return: None
         """
-        self.waiting_times = np.add(self.waiting_times, waiting_time)
+        self.waiting_times += waiting_time
 
-    def add_vehicles(self, num_vehicles : int = None) -> None:
+    def add_vehicles(self, num_vehicles: int = None) -> None:
         """
         Adds a set number of vehicles to the queue for this direction
 
@@ -40,13 +78,15 @@ class Direction:
 
         If num_vehicles is left empty, then it adds vehicles based on a normal distribution around the avg flow
 
-        :param num_vehicles: number of vehicles to be added
+        :param num_vehicles: number of vehicles to be added. Must be a positive integer
         :return: None
         """
-
-        if num_vehicles is None:
-            num_vehicles = abs(np.floor(np.random.normal(self.avg_flow)))
-        self.waiting_times.append([0] * num_vehicles)
+        if num_vehicles is not None:
+            assert isinstance(num_vehicles, int) and num_vehicles >= 0, \
+                "Number of vehicles must be an integer greater than or equal to zero!"
+        else:
+            num_vehicles = int(abs(np.floor(np.random.normal(self.avg_flow))))
+        np.concatenate((self.waiting_times, [0] * num_vehicles))
 
     def cycle_volume(self) -> int:
         """
@@ -58,8 +98,8 @@ class Direction:
 
         :return: cycle volume as described
         """
-        curr_total = sum(self.waiting_times)
-        req_total = curr_total // 2
+        curr_total = np.sum(self.waiting_times)
+        req_total = int(curr_total * self.cycle_size)
         volume = 0
         i = 0
         while curr_total > req_total:
