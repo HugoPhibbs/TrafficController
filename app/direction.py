e990c02acccacd5a21cf21a1517a751a114b1a31
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2022-01-12 13:02:13 +1300

    Update direction.py
    Added documentation
    
    Added getters and setters
    
    renamed empty to cycle to avoid confusion
    
    refactored code for maintainability

--- app/direction.py
+++ app/direction.py
@@ -4,9 +4,21 @@ from numpy import ndarray
 
 
 class Direction:
+    """
+    Class representing a Direction object.
+
+    Essentially, a direction is one way a vehicle can enter an intersection.
+
+    Directions form nodes in a circular linked list that makes up a complete intersection
+
+    Attributes
+        next: the next Direction object in the linked list as described
+        waiting_times: ndarray representing the waiting times of each of the vehicles in this intersection
+        name: str for the name of this intersection
+    """
     next = None
-    __cycle_size: float
-    waiting_times: ndarray
+    waiting_times: ndarray = None
+    __name: str = None
 
     def __init__(self, _name: str, _waiting_times: list = [], _avg_flow: int = 0, _cycle_size: int = 0.5):
         """
@@ -23,11 +35,16 @@ class Direction:
         self.cycle_size = _cycle_size
 
     @property
-    def cycle_size(self):
+    def cycle_size(self) -> float:
+        """
+        Getter method for the cycle size of this Direction object
+
+        :return: float for cycle size
+        """
         return self.__cycle_size
 
     @cycle_size.setter
-    def cycle_size(self, val: float):
+    def cycle_size(self, val: float) -> None:
         """
         Setter for the cycle size of this Direction
 
@@ -39,21 +56,63 @@ class Direction:
         assert isinstance(val, float) and 0 < val < 1
         self.__cycle_size = val
 
-    def empty(self, p) -> float:
+    @property
+    def cum_waiting_time(self) -> int:
+        """
+        Finds the total waiting time for this Direction
+
+        :return: int for the total waiting time of this intersection
+        """
+        return np.sum(self.waiting_times)
+
+    @property
+    def name(self) -> str:
+        """
+        Getter method for the name of this Direction object
+
+        :return: str for the name as described
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        """
+        Setter method for the name of this Direction object
+
+        :param new_name: str for the new name to be set
+        :return: None
+        """
+        self.__name = new_name
+
+    @property
+    def is_empty(self) -> bool:
+        """
+        Finds if this direction is empty of vehicles or not
+        :return:
+        """
+        return self.num_vehicles == 0
+
+    def cycle(self, p, should_sleep : bool = False) -> float:
         """
         Empties this direction for the required volume as specified by self.cycle_volume()
 
         :param p: the required time for a single vehicle to exit the intersection given a green light
+        :param should_sleep bool if a user would like to sleep the application in real time to reflect the passing of
+        traffic through the intersection. Useful for doing integration and performance testing
         :return: float for the total length of this cycle for this direction. Used to add waiting times to the rest of
         vehicles that are still waiting their turn
         """
-        cycle_volume = self.cycle_volume()
-        cycle_durr = cycle_volume * p
-        self.waiting_times = self.waiting_times[cycle_volume:]
-        sleep(cycle_durr)
-        return cycle_durr
+        cycle_durr = 0
+        if not self.is_empty:
+            cycle_volume = self.cycle_volume()
+            cycle_durr = cycle_volume * p
+            self.waiting_times = self.waiting_times[cycle_volume:]
+            if should_sleep:
+                sleep(cycle_durr)
+        return cycle_durr + 2  # To account for time to switch between directions
 
-    def __len__(self) -> int:
+    @property
+    def num_vehicles(self) -> int:
         """
         Returns the number of vehicles in this direction
 
@@ -98,7 +157,7 @@ class Direction:
 
         :return: cycle volume as described
         """
-        curr_total = np.sum(self.waiting_times)
+        curr_total = self.cum_waiting_time
         req_total = int(curr_total * self.cycle_size)
         volume = 0
         i = 0
